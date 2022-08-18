a810cdd05348c3bad81f0e5a8d9c41d7be1602d1
ReeceWilkinson <61788558+ReeceWilkinson@users.noreply.github.com>
2022-08-18 20:49:01 +0100

    Rename DOTWCompact.py to Challenge4-DOTWCompact.py

--- DOTWCompact.py
+++
@@ -1,3 +0,0 @@
-import datetime
-
-print(datetime.date(int(input("What year is it? ")) , int(input("What is the number month. E.G. febuary = 2 and June = 6. ")) , int(input("What day is it? "))).strftime("%A"))
